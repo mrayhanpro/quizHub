b7a79743685c19805da4082716604f17719a61bb
mrayhan-gh <muhammadrayhan.oe@gmail.com>
2023-09-19 15:22:53 +0600

    Stats component styled perfectly

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -3,10 +3,20 @@ import './Stats.css'
 
 const Stats = () => {
     return (
-        <div className='stats'>
-            <h3>
-                This is stats component
-            </h3>
+        <div id='stats' className='stats'>
+
+            <h1>Stats:</h1>
+
+           <div id='total-correct-answer-container'className='state-container'>
+                <h3>Total correct answer:</h3>
+                <p className='total-correct-answer-number number-container'>00</p>
+           </div>
+
+           <div id='total-incorrect-answer-container' className='state-container'>
+                <h3>Total incorrect answer:</h3>
+                <p className='total-incorrect-answer-number number-container'>00</p>
+           </div>
+           
         </div>
     );
 };
